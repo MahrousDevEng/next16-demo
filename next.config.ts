506d06ab7bca2d8a34c7d2067d295fb2ff8d9a8d
Mohamed Mahrous <eng.mahrous17@gmail.com>
2025-11-01 01:28:57 +0200

    Implement posthog

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
